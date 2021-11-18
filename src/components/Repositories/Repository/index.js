f9cba3114623971c1c45891ea9561f491d33b01d
airan-rocha <rocha.airan@gmail.com>
2021-11-18 16:00:31 -0300

    link de acesso ao repositório alterado

--- src/components/Repositories/Repository/index.js
+++ src/components/Repositories/Repository/index.js
@@ -5,7 +5,7 @@ const Repository = ({repos}) => {
         <div className="repositorie-view">
             <p>{repos.name}</p>
             <p><strong>Full name:</strong> {repos.full_name}</p>
-            <p><strong>Link:</strong> <a target='_blank' rel="noreferrer" href={repos.html_url}>{repos.html_url}</a></p>
+            <p><a target='_blank' rel="noreferrer" href={repos.html_url}>Access Repository</a></p>
         </div>
     );
 }

3be8bf661b693518b2c4a01935b4cefa28ebec1c
airan-rocha <rocha.airan@gmail.com>
2021-11-18 21:03:33 -0300

    atualização do link blog

--- src/components/Header/InfoUser/index.js
+++ src/components/Header/InfoUser/index.js
@@ -7,7 +7,7 @@ const InfoUser = ({value}) => {
           <p className="login"><strong>Username:</strong> <a target='_blank' rel="noreferrer" href={value.html_url}>{value.login}</a></p>
           {value.company && <p><strong>Company:</strong> {value.company}</p>}
           {value.location && <p><strong>Location:</strong> {value.location}</p>}
-          {value.blog && <p><strong>Blog:</strong> {value.blog}</p>}
+          {value.blog && <p><strong>Blog:</strong> <a target='_blank' rel="noreferrer" href={value.blog}>{value.blog}</a></p>}
         </div>
     );
 }
